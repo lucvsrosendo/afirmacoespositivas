b07bd70d3465cc21bf0193e9fe8b977524d44e42
Lucas <rosendolucas@gmail.com>
2026-03-17 19:54:02 -0300

    Update affirmation.js

--- api/affirmation.js
+++ api/affirmation.js
@@ -1,3 +1,12 @@
-module.exports = (req, res) => {
-  res.status(200).json({ ok: "funcionando" });
-};
+const fs = require('fs')
+const path = require('path')
+
+export default function handler(req, res) {
+  const filePath = path.join(process.cwd(), 'api', 'frases.json')
+  
+  const frases = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+
+  const fraseAleatoria = frases[Math.floor(Math.random() * frases.length)]
+
+  res.status(200).json({ frase: fraseAleatoria })
+}
